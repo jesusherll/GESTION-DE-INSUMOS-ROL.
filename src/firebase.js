32e0db9bb2971f2c7c2c94e809f1abc7f0923caf
JesusHerll <jus00714@hotmail.com>
2025-09-23 22:09:40 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWAeEuE36bP2qty8zF4qd7aOQTBy0xRxE",
-  authDomain: "gestion-de-insumos-c478a.firebaseapp.com",
-  projectId: "gestion-de-insumos-c478a",
-  storageBucket: "gestion-de-insumos-c478a.appspot.com",
-  messagingSenderId: "861986713329",
-  appId: "1:861986713329:web:78d0bf5c0cc6f89dbd4350",
-  measurementId: "G-17ZBG2LN4N"
+  apiKey: "aqui tu api key",
+  authDomain: "tu dominio ",
+  projectId: "nombre del proyecto",
+  storageBucket: "la direccion de aplicativo",
+  messagingSenderId: "tu senderid",
+  appId: "el appid de tus plicacion en firetore",
+  measurementId: "tu ide de mesurament"
 };
 
 
